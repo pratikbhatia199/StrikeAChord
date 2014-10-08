c526a838ea455cad0ccf0e3997905c805bbdbaf5
pratikbhatia199 <pratik.bhatia.199@gmail.com>
2014-10-07 22:09:44 -0400

    Added usernames and a function for expiration date

--- data_scrape.py
+++ data_scrape.py
@@ -1,6 +1,12 @@
 from bs4 import BeautifulSoup
 import bs4
 import urllib2
+from datetime import timedelta
+from random import randrange
+import datetime
+
+
+
 
 def data_scraper():
     page = urllib2.urlopen("http://www.ultimate-guitar.com/top/top100_chords.htm")
@@ -72,20 +78,36 @@ def username_scraper():
             line_data = line.strip().split()
             print line_data
             if len(line_data) == 3:
-
                 username = line_data[2].split('@')
-
-                file_users.write('INSERT INTO basic_user (username, firstname, lastname) VALUES "'+username[0]+'", "'+line_data[0]+'" , "'+line_data[1] +'"); \n')
+                file_users.write('INSERT INTO basic_user (username, firstname, lastname) VALUES ("'+username[0]+'", "'+line_data[0]+'" , "'+line_data[1] +'"); \n')
+                file_users.write('INSERT INTO basic_user (username, firstname, lastname) VALUES ("'+username[0]+'", "'+line_data[0]+'" , "'+line_data[1] +'"); \n')
             if len(line_data) == 4:
-
                 username = line_data[3].split('@')
-                file_users.write('INSERT INTO basic_user (username, firstname, lastname) VALUES "'+username[0]+'", "'+line_data[0]+'" , "'+line_data[2] +'"); \n')
+                file_users.write('INSERT INTO basic_user (username, firstname, lastname) VALUES ("'+username[0]+'", "'+line_data[0]+'" , "'+line_data[2] +'"); \n')
 
 
 
 
+def random_date(start, end):
+    """
+    This function will return a random datetime between two datetime
+    objects.
+    """
+    delta = end - start
+    print delta
+    int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+    print int_delta
+    random_second = randrange(int_delta)
+    return start + timedelta(seconds=random_second)
+
+def generate_day_between_today_and_past_three_months():
+    today = datetime.date.today()
+    last_three_months = today - datetime.timedelta(days=90)
+    print today, last_three_months
+    random_date_val = random_date(last_three_months, today)
+    print random_date_val
 
 
-
-username_scraper()
-data_scraper()
+#username_scraper()
+#data_scraper()
+generate_day_between_today_and_past_three_months()
